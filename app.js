c770b6d0f21172fce67855070722e16fac7b7aa1
Fr4n0m <fran11799@outlook.com>
2023-12-18 18:08:26 +0100

    add app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer( (req, res) => {
+
+    res.setHeader("Content-Type", "text/html; charset=utf-8")
+    res.end("<h1>Hola mundó</h1>")
+
+});
+
+server.listen(PORT, () => {
+
+    console.log('Server running at http:localhost:${PORT}')
+});
